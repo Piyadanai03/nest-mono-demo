acf07791d410da0faf3c2198aa966be365149d3b
Piyadanai03 <piyadnai.k2546@gmail.com>
2025-12-03 18:46:44 +0700

    fix: port api gateway

--- backend/apps/api-gateway/src/main.ts
+++ backend/apps/api-gateway/src/main.ts
@@ -6,11 +6,11 @@ import { GlobalExceptionFilter } from '@app/shared-lib/logging/exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
-  await app.listen(3000);
+  await app.listen(3001);
 
   app.useLogger(app.get(Logger));
   app.useGlobalFilters(new GlobalExceptionFilter(app.get(Logger)));
 
-  console.log(`🚀 API Gateway is running on: http://localhost:3000`);
+  console.log(`🚀 API Gateway is running on: http://localhost:3001`);
 }
 bootstrap();
